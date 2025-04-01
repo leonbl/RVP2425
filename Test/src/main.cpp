d8a38223f78ae4c34d98185569c3f1259a6aae30
leonbl <leon.blazic@gmail.com>
2025-04-01 14:51:07 +0200

    PWM diode

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
-#define VKLOP 0 // definiramo novo oznako
+#define VKLOP 0  // definiramo novo oznako
 #define IZKLOP 1 // prvajalnik bo pred začetkom prevajanja povsod
                  // v programu IZKLOP zamenjal z 1
 
 // funkcija, ki se izvede samo enkrat
-void setup() {
-  pinMode(13,OUTPUT); // nastavimo smer prenosa podatkov na pinu 13
+void setup()
+{
+  pinMode(10, OUTPUT); // nastavimo smer prenosa podatkov na pinu 13
 }
 
 // funkcija, ki svojo kodo izvaja neprestano
-void loop() {
-  digitalWrite(13,VKLOP); // na pin 13 damo logično stanje 0
-  delay(1000);
-  digitalWrite(13,IZKLOP);
-  delay(1000);
+void loop()
+{
+  for (int n = 0; n < 256; n++)
+  {
+    analogWrite(10, n); // PWM nstavim na 0 (maksimalno 255)
+    delay(4);
+  }
 }
