88b989b82fa9a595efdbde52eebda7b47ac55648
leonbl <leon.blazic@gmail.com>
2025-04-15 14:43:54 +0200

    update

--- Temperatura/src/main.cpp
+++ Temperatura/src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop()
+float measureTemp()
 {
   uint32_t nap = analogRead(12);
-  float resist = 10000 *(((float)4096/(float)nap)-1.0);
+  float resist = 10000 * (((float)4096 / (float)nap) - 1.0);
   float steinhart;
   steinhart = resist / 10000;
   steinhart = log(steinhart);
@@ -16,8 +17,17 @@ void loop()
   steinhart = 1.0 / steinhart;
   float tempK = steinhart;
   float tempC = tempK - 273.15;
+  return tempC;
+}
 
-  Serial.println(tempC);
+void loop()
+{
+  float tempSum = 0;
+  for (int n = 0; n < 10; n++)
+  {
+    tempSum += measureTemp();
+  }
+  tempSum /= 10;
+  Serial.println(tempSum);
   delay(500);
-
 }
