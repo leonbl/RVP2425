3cf241f9d4a7da01a690876c1db716a06be5ccbb
leonbl <leon.blazic@gmail.com>
2025-05-08 13:53:54 +0200

    Update main.cpp

--- SerialProcessingArduino/src/main.cpp
+++ SerialProcessingArduino/src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
+Napiši program ZA vklapljanje ledic!
+
+
 char incomingByte;
 void setup() {
   Serial.begin(9600);
+  pinMode(10,OUTPUT);
 }
 void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte);
+    Serial.print(incomingByte);
+    if(incomingByte == 'B'){
+      Serial.print('C');
+      digitalWrite(10,!digitalRead(10));
+    }
   }
 }
