530594855c63443117144b87f9377145c034245a
leonbl <leon.blazic@gmail.com>
2025-05-12 10:06:23 +0200

    Update main.cpp

--- SerialProcessingArduino/src/main.cpp
+++ SerialProcessingArduino/src/main.cpp
@@ -1,20 +1,45 @@
 #include <Arduino.h>
-Napiši program ZA vklapljanje ledic!
 
-
-char incomingByte;
-void setup() {
+char ib;
+void setup()
+{
   Serial.begin(9600);
-  pinMode(10,OUTPUT);
+  pinMode(10, OUTPUT);
 }
-void loop() {
-  if (Serial.available() > 0) {
+char buf[4];
+int cnt = 0;
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
     // read the incoming byte:
-    incomingByte = Serial.read();
-    Serial.print(incomingByte);
-    if(incomingByte == 'B'){
-      Serial.print('C');
-      digitalWrite(10,!digitalRead(10));
+    ib = Serial.read();
+    if (ib == '0' || ib == '1')
+    {
+      buf[cnt] = ib;
+      cnt++;
+      Serial.print(ib);
+    }
+    else if (ib == '\n')
+    {
+      if (buf[0] == '0')
+        digitalWrite(10, HIGH);
+      else
+        digitalWrite(10, HIGH);
+      if (buf[1] == '0')
+        digitalWrite(11, HIGH);
+      else
+        digitalWrite(11, HIGH);
+      if (buf[2] == '0')
+        digitalWrite(12, HIGH);
+      else
+        digitalWrite(12, HIGH);
+      if (buf[3] == '0')
+        digitalWrite(13, HIGH);
+      else
+        digitalWrite(13, HIGH);
+      cnt = 0;
     }
   }
 }
